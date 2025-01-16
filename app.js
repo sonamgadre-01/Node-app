7e4bae30c725ebfa09b7a03dbb148d332e8a2ea0
Mritunjay Paswan <mritunjay@manthanitsolutions.in>
2025-01-16 14:26:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: "Hello from the server"
+    })
+})
+
+
+app.listen(8080, () => console.log(`Server is running on port 8080`))
